7e73c9e847c29f2a4ee8428087e9416b6286f6d9
Pavel Lysenko <plysenko@plysenko.local>
2022-02-05 13:06:38 +0600

    [CR] Configured tagliatelle linter for snake

--- internal/storage/models/creative.go
+++ internal/storage/models/creative.go
@@ -8,22 +8,22 @@ type Creative struct {
 }
 
 type SlotCreative struct {
-	SlotID     ID `db:"slot_id" json:"slot_id"`         //nolint: tagliatelle
-	CreativeID ID `db:"creative_id" json:"creative_id"` //nolint: tagliatelle
+	SlotID     ID `db:"slot_id" json:"slot_id"`
+	CreativeID ID `db:"creative_id" json:"creative_id"`
 }
 
 type Impression struct {
 	ID         ID        `db:"ID" json:"id"`
-	SlotID     ID        `db:"slot_id" json:"slot_id"`         //nolint: tagliatelle
-	CreativeID ID        `db:"creative_id" json:"creative_id"` //nolint: tagliatelle
-	SegmentID  ID        `db:"segment_id" json:"segment_id"`   //nolint: tagliatelle
+	SlotID     ID        `db:"slot_id" json:"slot_id"`
+	CreativeID ID        `db:"creative_id" json:"creative_id"`
+	SegmentID  ID        `db:"segment_id" json:"segment_id"`
 	Time       time.Time `db:"time" json:"time"`
 }
 
 type Conversion struct {
 	ID         ID        `db:"ID"`
-	SlotID     ID        `db:"slot_id" json:"slot_id"`         //nolint: tagliatelle
-	CreativeID ID        `db:"creative_id" json:"creative_id"` //nolint: tagliatelle
-	SegmentID  ID        `db:"segment_id" json:"segment_id"`   //nolint: tagliatelle
+	SlotID     ID        `db:"slot_id" json:"slot_id"`
+	CreativeID ID        `db:"creative_id" json:"creative_id"`
+	SegmentID  ID        `db:"segment_id" json:"segment_id"`
 	Time       time.Time `db:"time" json:"time"`
 }
