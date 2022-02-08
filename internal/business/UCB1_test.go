03c69d76863e52d7897b9a1f8fd73798737c7c58
Pavel Lysenko <plysenko@plysenko.local>
2022-02-08 17:02:17 +0600

    [CR] Implemented simple tests for UCB1

---
+++ internal/business/UCB1_test.go
@@ -0,0 +1,109 @@
+package business
+
+import (
+	"testing"
+
+	"github.com/pls87/creative-rotation/internal/storage/models"
+	"github.com/stretchr/testify/require"
+)
+
+type step struct {
+	action      string
+	creative    models.ID
+	from        []models.ID
+	zero, total int
+}
+
+var stats = []models.Stats{
+	{CreativeID: 1, SlotID: 1, SegmentID: 1, Impressions: 0, Conversions: 0},
+	{CreativeID: 3, SlotID: 1, SegmentID: 1, Impressions: 0, Conversions: 0},
+	{CreativeID: 5, SlotID: 1, SegmentID: 1, Impressions: 0, Conversions: 0},
+	{CreativeID: 9, SlotID: 1, SegmentID: 1, Impressions: 0, Conversions: 0},
+	{CreativeID: 2, SlotID: 1, SegmentID: 1, Impressions: 0, Conversions: 0},
+}
+
+var steps = []step{
+	{action: "imp", creative: 5, zero: 4, total: 1},
+	{action: "imp", creative: 1, zero: 3, total: 2},
+	{action: "imp", creative: 2, zero: 2, total: 3},
+	{action: "conv", creative: 5, zero: 2, total: 3},
+	{action: "next", from: []models.ID{3, 9}, zero: 2, total: 3},
+	{action: "next", from: []models.ID{3, 9}, zero: 2, total: 3},
+	{action: "next", from: []models.ID{3, 9}, zero: 2, total: 3},
+	{action: "imp", creative: 9, zero: 1, total: 4},
+	{action: "next", from: []models.ID{3}, zero: 1, total: 4},
+	{action: "next", from: []models.ID{3}, zero: 1, total: 4},
+	{action: "conv", creative: 9, zero: 1, total: 4},
+	{action: "imp", creative: 3, zero: 0, total: 5},
+}
+
+func imp(creative models.ID, stats []models.Stats) []models.Stats {
+	for i, v := range stats {
+		if v.CreativeID == creative {
+			v.Impressions++
+			stats[i] = v
+		}
+	}
+	return stats
+}
+
+func conv(creative models.ID, stats []models.Stats) []models.Stats {
+	for i, v := range stats {
+		if v.CreativeID == creative {
+			v.Conversions++
+			stats[i] = v
+		}
+	}
+	return stats
+}
+
+func find(ids []models.ID, id models.ID) bool {
+	for _, i := range ids {
+		if i == id {
+			return true
+		}
+	}
+	return false
+}
+
+func TestUSB1Playback(t *testing.T) {
+	stats := stats
+	t.Helper()
+	for _, s := range steps {
+		switch s.action {
+		case "imp":
+			stats = imp(s.creative, stats)
+		case "conv":
+			stats = conv(s.creative, stats)
+		case "next":
+			c := NextCreative(stats)
+			require.Truef(t, find(s.from, c), "%d wasn't found in %v", c, s.from)
+		}
+		zero, total := aggregate(stats)
+		require.Equal(t, s.zero, len(zero))
+		require.Equal(t, uint64(s.total), total)
+	}
+}
+
+func TestAggregate(t *testing.T) {
+	stats := []models.Stats{
+		{CreativeID: 1, SlotID: 1, SegmentID: 1, Impressions: 10, Conversions: 3},
+		{CreativeID: 3, SlotID: 1, SegmentID: 1, Impressions: 7, Conversions: 1},
+		{CreativeID: 5, SlotID: 1, SegmentID: 1, Impressions: 0, Conversions: 0},
+		{CreativeID: 9, SlotID: 1, SegmentID: 1, Impressions: 0, Conversions: 0},
+		{CreativeID: 2, SlotID: 1, SegmentID: 1, Impressions: 5, Conversions: 2},
+	}
+	expectedZeroStats := []models.Stats{
+		{CreativeID: 5, SlotID: 1, SegmentID: 1, Impressions: 0, Conversions: 0},
+		{CreativeID: 9, SlotID: 1, SegmentID: 1, Impressions: 0, Conversions: 0},
+	}
+
+	var expectedTotal uint64 = 22
+
+	zeroStats, total := aggregate(stats)
+
+	require.Equalf(t, expectedTotal, total, "Impressions are not aggregated correctly")
+	require.Equalf(t, len(expectedZeroStats), len(zeroStats), "Expected %d empty stats but got: %d",
+		len(expectedZeroStats), len(zeroStats))
+	require.Equalf(t, expectedZeroStats, zeroStats, "Empty stats aggregated incorrectly")
+}
