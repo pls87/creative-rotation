60fc82c1d050bfc3035ddc06c53e4103c5af0a0c
Pavel Lysenko <plysenko@plysenko.local>
2022-02-15 01:42:29 +0600

    [CR] Updated script for running integration tests

--- scripts/run-integration-test.sh
+++ scripts/run-integration-test.sh
@@ -2,6 +2,4 @@
 
 cd deployments
 
-trap "docker-compose -p crtest -f docker-compose.yaml -f docker-compose.tests.yaml down" EXIT
-
 docker-compose -p crtest -f docker-compose.yaml -f docker-compose.test.yaml run integration-tests
